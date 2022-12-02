a3e99ff2491c848ae4be05bc7309b67bb207c415
Evgenii Koriakin <eakoriakin@yandex.ru>
2022-12-02 08:33:52 +0400

    Update PageClient.

--- api/clients/page.client.js
+++ api/clients/page.client.js
@@ -7,12 +7,12 @@ export class PageClient {
     return this.axios.$get(
       // We cannot request files directly from GitHub because of CORS limitation.
       // Preflight OPTIONS request to GitHub always returns 403 error preventing a file from loading.
-      // `https://raw.githubusercontent.com/ionic-selectable/ionic-selectable-pages/main/pages/${pageName}.md`
+      // `https://raw.githubusercontent.com/eakoriakin/ionic-selectable-pages/main/pages/${pageName}.md`
       `https://unpkg.com/ionic-selectable-pages/pages/${pageName}.md`
     );
   }
 
   getPageUrl(pageName) {
-    return `https://github.com/ionic-selectable/ionic-selectable-pages/blob/main/pages/${pageName}.md`;
+    return `https://github.com/eakoriakin/ionic-selectable-pages/blob/main/pages/${pageName}.md`;
   }
 }
